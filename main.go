32ebc53b2e9fe7fc868c732efc49a8ab4e9c21d8
efzedel16 <efzedel16@outlook.com>
2021-05-11 06:36:50 +0700

    Reconnect to silih_a3 database

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/silih_a3?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+}
