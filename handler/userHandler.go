0d1694c872e468a4d986fa97e4d389db86f9a2f2
efzedel16 <efzedel16@outlook.com>
2021-05-12 06:17:11 +0700

    Handling user responses

--- handler/userHandler.go
+++ handler/userHandler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"silih_a3/helper"
 	"silih_a3/user"
 )
 
@@ -16,9 +17,23 @@ func NewUserHandler(userService user.Service) *userHandler {
 
 func (h *userHandler) SignUpUser(c *gin.Context) {
 	var input user.SignUpUserInput
-	newUser, err := h.userService.SignUpUser(input)
+	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, newUser)
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
+
+	newUser, err := h.userService.SignUpUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, token)
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
 }
