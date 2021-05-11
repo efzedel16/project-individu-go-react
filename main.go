2dbe3c0c3da666369d8e2773ddfe42383f93c027
efzedel16 <efzedel16@outlook.com>
2021-05-11 10:26:39 +0700

    Create a userRepository

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"silih_a3/user"
 )
 
 func main() {
 	dsn := "root:root@tcp(127.0.0.1:3306)/silih_a3?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
+	userRepository := user.NewRepository(db)
 }
