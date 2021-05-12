79c83ed33b5b63edff8facb8817dc6f80dfb1a9f
efzedel16 <efzedel16@outlook.com>
2021-05-12 08:17:31 +0700

    Create userInput, userHandler, userService, userRepository

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"silih_a3/user"
 )
 
 func main() {
+	//dsn := "root:root@tcp(127.0.0.1:3306)/silih_a3?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//if err != nil {
+	//	log.Fatal(err.Error())
+	//}
+	//
+	//fmt.Println("Connect DB")
+	//
+	//var users []user.User
+	//db.Find(&users)
+	//for _, user := range users {
+	//	fmt.Println(user.Id)
+	//	fmt.Println(user.FirstName)
+	//	fmt.Println(user.LastName)
+	//}
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context)  {
 	dsn := "root:root@tcp(127.0.0.1:3306)/silih_a3?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connect DB")
-
 	var users []user.User
 	db.Find(&users)
-	for _, user := range users {
-		fmt.Println(user.Id)
-		fmt.Println(user.FirstName)
-		fmt.Println(user.LastName)
-	}
+
+	c.JSON(http.StatusOK, users)
 }
