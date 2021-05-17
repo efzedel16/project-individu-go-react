f5b51d42c18b96ab334c5f651e8dfd5477628815
Muhammad Fauzul Hanif <efzedel16@outlook.com>
2021-05-18 06:56:52 +0700

    Create donationEntity - donation endpoint

--- donation/donationEntity.go
+++ donation/donationEntity.go
@@ -1,1 +1,28 @@
 package donation
+
+import "time"
+
+type Donation struct {
+	Id               int
+	UserId           int
+	Name             string
+	ShortDescription string
+	LongDescription  string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	DonationImages   []DonationImage
+}
+
+type DonationImage struct {
+	Id         int
+	DonationId int
+	FileName   string
+	IsPrimary  string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
