4876bab83696c35b8131ca6942bd0f6521f7dd31
efzedel16 <efzedel16@outlook.com>
2021-05-18 06:58:54 +0700

    Create FindAllDonations, FindDonationsByUserId - donation endpoint

--- donation/donationRepository.go
+++ donation/donationRepository.go
@@ -1,1 +1,36 @@
 package donation
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAllDonations() ([]Donation, error)
+	FindDonationsByUserId(userId int) ([]Donation, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindAllDonations() []Donation {
+	var donations []Donation
+	err := r.db.Preload("DonationImages", "donation_images.is_primary = 1").Find(&donations).Error
+	if err != nil {
+		return donations
+	}
+
+	return donations
+}
+
+func (r *repository) FindDonationsByUserId(userId int) []Donation {
+	var donations []Donation
+	err := r.db.Where("user_id = ?", userId).Preload("DonationImages", "donation_images.is_primary = 1").Find(&donations).Error
+	if err != nil {
+		return donations
+	}
+
+	return donations
+}
