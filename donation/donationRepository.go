554925f50a0c6a5c56054685aa72d8df5b8f3d83
efzedel16 <efzedel16@outlook.com>
2021-05-18 10:07:26 +0700

    Fix donationRepository - donation endpoint

--- donation/donationRepository.go
+++ donation/donationRepository.go
@@ -15,22 +15,22 @@ func NewRepository(db *gorm.DB) *repository {
 	return &repository{db}
 }
 
-func (r *repository) FindAllDonations() []Donation {
+func (r *repository) FindAllDonations() ([]Donation, error) {
 	var donations []Donation
 	err := r.db.Preload("DonationImages", "donation_images.is_primary = 1").Find(&donations).Error
 	if err != nil {
-		return donations
+		return donations, err
 	}
 
-	return donations
+	return donations, nil
 }
 
-func (r *repository) FindDonationsByUserId(userId int) []Donation {
+func (r *repository) FindDonationsByUserId(userId int) ([]Donation, error) {
 	var donations []Donation
 	err := r.db.Where("user_id = ?", userId).Preload("DonationImages", "donation_images.is_primary = 1").Find(&donations).Error
 	if err != nil {
-		return donations
+		return donations, err
 	}
 
-	return donations
+	return donations, nil
 }
