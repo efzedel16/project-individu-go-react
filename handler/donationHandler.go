ab168b84eda1d921c3ff846c2e0a05cda8088259
efzedel16 <efzedel16@outlook.com>
2021-05-22 07:55:27 +0700

    Create GetDonation - donation endpoint

--- handler/donationHandler.go
+++ handler/donationHandler.go
@@ -28,3 +28,23 @@ func (h *donationHandler) GetDonations(c *gin.Context) {
 	response := helper.APIResponse("Successfully to get donations", http.StatusOK, "success", donation.DonationsFormat(donations))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *donationHandler) GetDonation(c *gin.Context) {
+	var input donation.DonationIdInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get donation details", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	donationDetails, err := h.donationService.GetDonationById(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get donation details", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Successfully to get donation details", http.StatusOK, "success", donation.DonationDetailsFormat(donationDetails))
+	c.JSON(http.StatusOK, response)
+}
