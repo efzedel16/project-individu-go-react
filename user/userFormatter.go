3ff2f138b9f7e6559861c165072e0e04fd83cebc
efzedel16 <efzedel16@outlook.com>
2021-05-22 16:32:04 +0700

    Create UserFormatter, UserFormat & fix userFormatter - user endpoint

--- user/userFormatter.go
+++ user/userFormatter.go
@@ -1,21 +1,43 @@
 package user
 
-type FormatterResponse struct {
+type UserFormatter struct {
 	Id        int    `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
+	Avatar    string `json:"avatar"`
+}
+
+type Formatter struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Avatar    string `json:"avatar"`
 	Token     string `json:"token"`
 }
 
-func Format(user User, token string) FormatterResponse {
-	formatter := FormatterResponse{
+func UserFormat(user User) UserFormatter {
+	userFormatter := UserFormatter{
 		Id:        user.Id,
 		FirstName: user.FirstName,
 		LastName:  user.LastName,
 		Email:     user.Email,
+		Avatar:    user.Avatar,
+	}
+
+	return userFormatter
+}
+
+func Format(user User, token string) Formatter {
+	userFormatter := Formatter{
+		Id:        user.Id,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Avatar:    user.Avatar,
 		Token:     token,
 	}
 
-	return formatter
+	return userFormatter
 }
