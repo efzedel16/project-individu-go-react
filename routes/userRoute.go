53564d66ac4af741934c2fc6fd2d3add8ae3f374
efzedel16 <efzedel16@outlook.com>
2021-05-25 15:47:38 +0700

    User route clean code

--- routes/userRoute.go
+++ routes/userRoute.go
@@ -2,29 +2,29 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 	"silih_a3/auth"
 	"silih_a3/config"
+	"silih_a3/handler"
+	"silih_a3/middleware"
 	"silih_a3/user"
 )
 
 var (
-	DB             *gorm.DB = config.ConnectDB()
-	userRepository          = user.NewUserRepository(DB)
-	userService             = user.NewUserService(userRepository)
-	authService             = auth.NewService()
-	userHandler             = user.NewUserHandler(userService, authService)
+	DB             = config.ConnectDB()
+	userRepository = user.NewUserRepository(DB)
+	userService    = user.NewUserService(userRepository)
+	authService    = auth.NewService()
+	userHandler    = handler.NewUserHandler(userService, authService)
 )
 
 func UserRoute(r *gin.Engine) {
-	//r.GET("/users", auth.AuthMiddleware(userService, authService), u)
+	r.GET("/users", userHandler.ShowAllUsers)
 
-	//user := r.Group("/users")
-	{
-		r.GET("/users/:id", userHandler.GetUser)
-		r.POST("/users/signup", userHandler.SignUpUser)
-		r.POST("/users/signin", userHandler.SignInUser)
-		r.POST("/users/email_checker", userHandler.CheckEmailAvailability)
-		r.POST("/users/avatar", auth.AuthMiddleware(authService, userService), userHandler.UploadAvatar)
-	}
+	user := r.Group("/users")
+	user.GET("/:id", middleware.AuthMiddleware(authService, userService), userHandler.GetUser)
+	user.POST("/register", userHandler.SignUpUser)
+	user.POST("/login", middleware.AuthMiddleware(authService, userService), userHandler.SignInUser)
+	user.POST("/email_checker", userHandler.CheckEmailAvailability)
+	r.POST("/avatar", middleware.AuthMiddleware(authService, userService), userHandler.UploadAvatar)
+
 }
