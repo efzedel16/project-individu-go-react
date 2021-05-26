1b3493b98853d804918bffee1351273e9bad2251
efzedel16 <efzedel16@outlook.com>
2021-05-26 21:51:22 +0700

    Fix helper

--- helper/helper.go
+++ helper/helper.go
@@ -3,21 +3,21 @@ package helper
 import "github.com/go-playground/validator/v10"
 
 type Response struct {
-	Meta Meta 		 `json:"meta"`
+	Meta Meta        `json:"meta"`
 	Data interface{} `json:"data"`
 }
 
 type Meta struct {
 	Message string `json:"message"`
-	Code 	int    `json:"code"`
-	Status 	string `json:"status"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
 }
 
 func APIResponse(message string, code int, status string, data interface{}) Response {
-	meta:= Meta{
+	meta := Meta{
 		Message: message,
-		Code: code,
-		Status: status,
+		Code:    code,
+		Status:  status,
 	}
 
 	jsonResponse := Response{
@@ -28,7 +28,7 @@ func APIResponse(message string, code int, status string, data interface{}) Resp
 	return jsonResponse
 }
 
-func SignUpValidationErrorFormat(err error) []string {
+func ErrorValidationRequiredFormat(err error) []string {
 	var errors []string
 	for _, err := range err.(validator.ValidationErrors) {
 		errors = append(errors, err.Error())
