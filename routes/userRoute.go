91c366098470cc4ae2e0be8dda9ceddf22e1f21d
efzedel16 <efzedel16@outlook.com>
2021-05-27 00:25:01 +0700

    Fix userRoute

--- routes/userRoute.go
+++ routes/userRoute.go
@@ -4,27 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"silih_a3/auth"
 	"silih_a3/config"
-	"silih_a3/handler"
+	"silih_a3/handlers"
 	"silih_a3/middleware"
-	"silih_a3/user"
+	"silih_a3/repositories"
+	"silih_a3/services"
 )
 
 var (
-	DB             = config.ConnectDB()
-	userRepository = user.NewUserRepository(DB)
-	userService    = user.NewUserService(userRepository)
-	authService    = auth.NewService()
-	userHandler    = handler.NewUserHandler(userService, authService)
+	userDb         = config.ConnectDB()
+	userRepository = repositories.NewUserRepository(userDb)
+	userService    = services.NewUserService(userRepository)
+	authService    = auth.NewAuthService()
+	userHandler    = handlers.NewUserHandler(userService, authService)
 )
 
 func UserRoute(r *gin.Engine) {
 	r.GET("/users", userHandler.ShowAllUsers)
 
 	user := r.Group("/users")
-	user.GET("/:id", middleware.AuthMiddleware(authService, userService), userHandler.GetUser)
-	user.POST("/register", userHandler.SignUpUser)
-	user.POST("/login", middleware.AuthMiddleware(authService, userService), userHandler.SignInUser)
+	user.GET("/:id", middleware.AuthMiddleware(authService, userService), userHandler.ShowUser)
+	user.POST("/signup", userHandler.SignUpUser)
+	user.POST("/signin", middleware.AuthMiddleware(authService, userService), userHandler.SignInUser)
 	user.POST("/email_checker", userHandler.CheckEmailAvailability)
-	r.POST("/avatar", middleware.AuthMiddleware(authService, userService), userHandler.UploadAvatar)
+	user.POST("/avatar", middleware.AuthMiddleware(authService, userService), userHandler.UploadAvatar)
 
 }
