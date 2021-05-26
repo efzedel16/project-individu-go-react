74ab7c063a087343982f2423aa8b9ca12e244247
efzedel16 <efzedel16@outlook.com>
2021-05-26 22:33:29 +0700

    Fix userEntity

--- entities/userEntity.go
+++ entities/userEntity.go
@@ -2,14 +2,14 @@ package entities
 
 import "time"
 
-type UserDB struct {
-	Id         int
-	FirstName  string
-	LastName   string
-	Email      string
-	Password   string
-	AvatarPath string
-	Role       string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+type User struct {
+	Id        int
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+	Avatar    string
+	Role      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
