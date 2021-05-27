40b7a4b91cdba603680a19a452502118f0369406
efzedel16 <efzedel16@outlook.com>
2021-05-27 21:45:29 +0700

    Fix & donation clean code

--- main.go
+++ main.go
@@ -6,28 +6,13 @@ import (
 )
 
 func main() {
-	//dsn := "root:root@tcp(127.0.0.1:3306)/silih_a3?charset=utf8mb4&parseTime=True&loc=Local"
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//}
+	r := gin.Default()
+	r.Static("/images", "./images")
 
-	//donationRepository := donation.NewDonationRepository(db)
-	//donationService := donation.NewDonationService(donationRepository)
-	//authService := auth.NewService()
-	//donationHandler := handler.NewDonationHandler(donationService)
+	routes.UserRoute(r)
+	routes.DonationRoute(r)
 
-	router := gin.Default()
-
-	//router.GET("/donations", donationHandler.GetAllDonations)
-	//router.Static("/images", "./images")
-
-	routes.UserRoute(router)
-
-	//donation := router.Group("/donations")
-	//donation.GET("/:id", donationHandler.GetDonation)
-
-	err := router.Run()
+	err := r.Run()
 	if err != nil {
 		return
 	}
