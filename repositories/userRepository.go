f74670cc1a00dea7ffe6c4ca80aad424c58c4bc1
efzedel16 <efzedel16@outlook.com>
2021-05-27 21:50:01 +0700

    Add UpdateUserById & DeleteUserById

--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -8,6 +8,8 @@ import (
 type UserRepository interface {
 	InsertUser(userData entities.User) (entities.User, error)
 	UpdateUser(userData entities.User) (entities.User, error)
+	UpdateUserById(userId int, updateData map[string]interface{}) (entities.User, error)
+	DeleteUserById(userId int) (string, error)
 	FindAllUsers() ([]entities.User, error)
 	FindUserById(userId int) (entities.User, error)
 	FindUserByEmail(userEmail string) (entities.User, error)
@@ -37,29 +39,50 @@ func (r *userRepository) UpdateUser(userData entities.User) (entities.User, erro
 	return userData, nil
 }
 
-func (r *userRepository) FindAllUsers() ([]entities.User, error) {
-	var users []entities.User
-	if err := r.userDb.Find(&users).Error; err != nil {
-		return users, err
+func (r *userRepository) UpdateUserById(userId int, updateData map[string]interface{}) (entities.User, error) {
+	var userData entities.User
+	if err := r.userDb.Model(&userData).Where("id = ?", userId).Updates(updateData).Error; err != nil {
+		return userData, err
 	}
 
-	return users, nil
+	if err := r.userDb.Where("id = ?", userId).Find(&userData).Error; err != nil {
+		return userData, err
+	}
+
+	return userData, nil
 }
 
-func (r *userRepository) FindUserById(id int) (entities.User, error) {
-	var user entities.User
-	if err := r.userDb.Where("id = ?", id).Find(&user).Error; err != nil {
-		return user, err
+func (r *userRepository) DeleteUserById(userId int) (string, error) {
+	if err := r.userDb.Where("id = ?", userId).Delete(&entities.User{}).Error; err != nil {
+		return "error", err
 	}
 
-	return user, nil
+	return "success", nil
+}
+
+func (r *userRepository) FindAllUsers() ([]entities.User, error) {
+	var usersData []entities.User
+	if err := r.userDb.Find(&usersData).Error; err != nil {
+		return usersData, err
+	}
+
+	return usersData, nil
+}
+
+func (r *userRepository) FindUserById(userId int) (entities.User, error) {
+	var userData entities.User
+	if err := r.userDb.Where("id = ?", userId).Find(&userData).Error; err != nil {
+		return userData, err
+	}
+
+	return userData, nil
 }
 
 func (r *userRepository) FindUserByEmail(userEmail string) (entities.User, error) {
-	var user entities.User
-	if err := r.userDb.Where("email = ?", userEmail).Find(&user).Error; err != nil {
-		return user, err
+	var userData entities.User
+	if err := r.userDb.Where("email = ?", userEmail).Find(&userData).Error; err != nil {
+		return userData, err
 	}
 
-	return user, nil
+	return userData, nil
 }
