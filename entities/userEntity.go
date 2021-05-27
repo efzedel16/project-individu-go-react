c1ed1e9633806b38442e4e32c75fb0ca1f163c72
efzedel16 <efzedel16@outlook.com>
2021-05-27 21:47:04 +0700

    Fix userEntity

--- entities/userEntity.go
+++ entities/userEntity.go
@@ -3,13 +3,13 @@ package entities
 import "time"
 
 type User struct {
-	Id        int
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
-	Avatar    string
-	Role      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id           int       `json:"id"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"password_hash"`
+	AvatarPath   string    `json:"avatar_path"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
