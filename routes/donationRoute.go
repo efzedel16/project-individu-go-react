4298934bdbb793e438c850950781f65d12227bd4
efzedel16 <efzedel16@outlook.com>
2021-05-27 22:41:50 +0700

    donationRoute comment

--- routes/donationRoute.go
+++ routes/donationRoute.go
@@ -1,24 +1,25 @@
 package routes
 
-import (
-	"github.com/gin-gonic/gin"
-	"silih_a3/config"
-	"silih_a3/handlers"
-	"silih_a3/repositories"
-	"silih_a3/services"
-)
-
-var (
-	donationDb         = config.ConnectDB()
-	donationRepository = repositories.NewDonationRepository(donationDb)
-	donationService    = services.NewDonationService(donationRepository)
-	// authServiceDonation = auth.NewAuthService()
-	donationHandler = handlers.NewDonationHandler(donationService)
-)
-
-func DonationRoute(r *gin.Engine) {
-	r.GET("/donations", donationHandler.ShowAllDonations)
-
-	donation := r.Group("/donations")
-	donation.GET("/:id", donationHandler.ShowDonation)
-}
+//
+// import (
+// 	"github.com/gin-gonic/gin"
+// 	"silih_a3/config"
+// 	"silih_a3/handlers"
+// 	"silih_a3/repositories"
+// 	"silih_a3/services"
+// )
+//
+// var (
+// 	donationDb         = config.ConnectDB()
+// 	donationRepository = repositories.NewDonationRepository(donationDb)
+// 	donationService    = services.NewDonationService(donationRepository)
+// 	// authServiceDonation = auth.NewAuthService()
+// 	donationHandler = handlers.NewDonationHandler(donationService)
+// )
+//
+// func DonationRoute(r *gin.Engine) {
+// 	r.GET("/donations", donationHandler.ShowAllDonations)
+//
+// 	donation := r.Group("/donations")
+// 	donation.GET("/:id", donationHandler.ShowDonation)
+// }
